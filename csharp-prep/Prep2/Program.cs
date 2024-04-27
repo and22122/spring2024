e120694d31337e8d1b4ccd8b81d83945f3a4781b
and22122 <and22122@byui.edu>
2024-04-26 19:04:12 -0600

    I did the prep2 assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        int grade = int.Parse(Console.ReadLine());
+
+        char letterGrade;
+
+        if (grade >= 90) {
+            letterGrade = 'A';
+        }
+        else if (grade >= 80) {
+            letterGrade = 'B';
+        }
+        else if (grade >= 70) {
+            letterGrade = 'C';
+        }
+        else if (grade >= 60) {
+            letterGrade = 'D';
+        }
+        else {
+            letterGrade = 'F';
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else {
+            Console.WriteLine("You failed, but I bet you'll ace it next time!");
+        }
     }
 }
