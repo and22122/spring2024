f4448a6195a710add46a7744bd7756ff654faa45
and22122 <and22122@byui.edu>
2024-05-06 10:07:16 -0600

    I modified the sandbox project.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,17 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randGen = new Random();
-        bool badNum = false;
+        Course course1 = new Course();
+        course1._courseCode = "CSE210";
+        course1._courseName = "Programming with Classes";
+        course1._creditHours = 2;
+        course1._color = "green";
 
-        for (int i = 0; i < 1000; i ++) {
-            int n = randGen.Next(1, 11);
-
-            if (n == 11) {
-                badNum = true;
-            }
-        }
-
-        Console.Write(badNum);
+        course1.Display();
     }
 }
