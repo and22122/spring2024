c7a4205837f5ecf1fa5196fb4a954b75557dfc77
and22122 <and22122@byui.edu>
2024-05-20 10:54:53 -0600

    Worked on the fraction program. Still needs work.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        List<Fraction> fractions = new List<Fraction>();
+        fractions.Add(new Fraction());
+        fractions.Add(new Fraction(5));
+        fractions.Add(new Fraction(3, 4));
+        fractions.Add(new Fraction(1, 3));
+        fractions.Add(new Fraction(3));
+        fractions.Add(new Fraction(3));
+
+        fractions[4].SetBottom(2);
+        fractions[5].SetTop(4);
+        fractions[5].SetBottom(4);
+
+        foreach (Fraction f in fractions)
+        {
+            Console.WriteLine(f.GetTop());
+            Console.WriteLine(f.GetBottom());
+            Console.WriteLine(f.GetFractionString());
+            Console.WriteLine(f.GetDecimalValue());
+        }
     }
 }
