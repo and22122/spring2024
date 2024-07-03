936a33fd13a73427e7bb593660f4af18209c02ec
and22122 <122041196+and22122@users.noreply.github.com>
2024-07-03 09:04:07 -0600

    Resetting changes to sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        NewClass n = new NewClass();
-        Console.WriteLine(n);
-        Console.WriteLine(n.ToString());
+        
     }
 }
