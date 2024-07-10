11fb6b2303d31c0dc6884b960d2e2a6d8cf76609
and22122 <122041196+and22122@users.noreply.github.com>
2024-07-10 10:12:56 -0600

    Some changes were made for debugging. There might be some oddities with how the computer treats data types.

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -7,19 +7,19 @@ class Program
         List<Order> orders = new List<Order>();
 
         orders.Add(new Order("Maher-shalal-hash-baz", "1234 Bible St.", "Zion", "The New Jerusalem", "The Kingdom of God"));
-        orders.Add(new Order("Joe Biden", "1600 Pennsylvania Avenue NW", "Washington", "DC", "USA"));
+        orders.Add(new Order("Joe Biden", "1600 Pennsylvania Avenue NW", "Washington", "DC", "US"));
 
         orders[0].addItem("The Holy Bible", 2, 10, 30);
         orders[0].addItem("Tribe of Israel", 3, 0, 13);
 
         orders[1].addItem("Trump to leave", 0, 0, 1);
-        orders[1].addItem("Belay that!", 1, 1000000, 300);
+        orders[1].addItem("Belay that!", 1, 10000, 300);
 
         foreach (Order o in orders)
         {
             Console.WriteLine(o.getPackingLabel());
             Console.WriteLine(o.getShippingLabel());
-            Console.WriteLine(o.getCost());
+            Console.WriteLine($"${o.getCost()}");
         }
     }
 }
